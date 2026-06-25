0b5765fbe89d3393cc903d11bb7383ae56d96891
K.Yoshida <yoshida@cosmo-w.co.jp>
2026-06-26 08:19:10 +0900

    リポジトリ作成中

--- src/employee/EmployeeRepository.java
+++ src/employee/EmployeeRepository.java
@@ -6,15 +6,22 @@ import java.util.*;
 public class EmployeeRepository {
 	private final List<Employee> employees = new ArrayList<>();
 	
-	/*
-	private void save(Employee employee) {
+	public void save(Employee employee) {
 		//リストに社員を追加する
+		employees.add(employee);
 	}
-	
-	private Optional<Employee> findById(String employeeId){
+
+	public Optional<Employee> findById(String employeeId){
 		//IDで社員を1件取得する
+		for(Employee emp:employees) {
+			if(emp.getEmployeeId().equals(employeeId)) {
+				return emp;
+			}
+		}
+		return null;
 	}
 	
+	/*
 	private List<Employee> findAll(){
 		//全社員を取得する（退職者を含む）
 	}
